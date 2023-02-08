b21ff42044505b16fbbdf703bcfef29b3450f0ac
lukelhi <coderlukelhi@gmail.com>
2023-02-08 18:13:00 +0800

    feat:initialize project structure

---
+++ AppCenter-manage/src/test/java/com/lhi/appcenter/manage/AppCenterManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhi.appcenter.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCenterManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ AppCenter-web/src/main/java/com/lhi/appcenter/web/AppCenterWebApplication.java
@@ -0,0 +1,13 @@
+package com.lhi.appcenter.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCenterWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCenterWebApplication.class, args);
+    }
+
+}
